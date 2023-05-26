48d540dd23ff36582fb9fb9234b94c2ef83d2474
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2023-05-26 23:55:15 +0200

    added precommit hooks and mypy

---
+++ README.md
@@ -0,0 +1,3 @@
+# Autoresolve
+
+An attempt to simplify legal jargon for people who encounter contracts and contract writing using LLMs.
--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 import streamlit as st
-import pathlib
-from langchain import OpenAI, PromptTemplate, LLMChain
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.chains.mapreduce import MapReduceChain
-from langchain.prompts import PromptTemplate
-from langchain.docstore.document import Document
-from langchain.chains.summarize import load_summarize_chain
-import os
 from dotenv import load_dotenv
+from langchain import OpenAI
+from langchain.chains.summarize import load_summarize_chain
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+
 load_dotenv()
 
 
 def summarize(input_string: str) -> str:
-    llm = OpenAI(temperature=0)
+    llm = OpenAI(temperature=0)  # type: ignore[call-arg]
     text_splitter = CharacterTextSplitter()
     contract = input_string
     texts = text_splitter.split_text(contract)
     docs = [Document(page_content=t) for t in texts]
     chain = load_summarize_chain(llm, chain_type="refine")
     return chain.run(docs)
+
 
 input_string = st.text_area("Please enter your text here:", "")
 if run_button := st.button("Run"):
